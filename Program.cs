f2152956dd92e36e70572f2b3030d93cef126ec2
MickaelHm <harmeau.mickael@gmail.com>
2018-12-19 14:59:04 +0100

    Correction

--- Program.cs
+++ Program.cs
@@ -14,30 +14,40 @@ namespace Condition3
             Console.WriteLine("Ecrivez votre âge svp");
             int age = int.Parse(Console.ReadLine());
             Console.WriteLine("Êtes vous un homme ou une femme");
-            string sexe = Console.ReadLine();
-            if (sexe == "homme"||sexe == "Homme")
+            string sex = Console.ReadLine();
+            if (sex == "homme" || sex == "Homme")
             {
-                if (age >=18)
+                if (age >= 18 && age < 120) // opérateur de comparaison pour rester dans la fourchette voulue
                 {
                     Console.WriteLine("Vous êtes un homme et vous êtes majeur.");
-
                 }
-                else
+                else if (age < 18 && age >= 1)
                 {
                     Console.WriteLine("Vous êtes un homme et vous êtes mineur.");
                 }
+                else // si la réponse est hors des valeurs voulues
+                {
+                    Console.WriteLine("Oops, il doit y avoir une erreur dans l'age");
+                }
             }
-            else
+            else if (sex == "femme" || sex == "Femme")
             {
-                if (age>=18)
+                if (age >= 18 && age < 120) // opérateur de comparaison pour rester dans la fourchette voulue
                 {
                     Console.WriteLine("vous êtes une femme et vous êtes majeure.");
                 }
-                else
+                else if (age < 18 && age >= 1)
                 {
                     Console.WriteLine("Vous êtes une femme et vous êtes mineure");
                 }
+                else // si la réponse est hors des valeurs voulues
+                {
+                    Console.WriteLine("Oops, il doit y avoir une erreur dans l'age");
+                }
             }
+            // si la réponse est hors des valeurs voulues
+            else{ Console.WriteLine("Oops, il doit y avoir une erreur dans le genre"); }
         }
     }
 }
+
